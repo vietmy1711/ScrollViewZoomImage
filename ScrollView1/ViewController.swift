50044b69ed7b0bb1e6c781d3bac20c895cae71df
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-06-01 15:11:38 +0700

    Add zoom image

--- ScrollView1/ViewController.swift
+++ ScrollView1/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imvImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = 10
     }
-
-
+    
+    
 }
 
+//MARK: - UIScrollViewDelegate
+
+extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imvImage
+    }
+}
