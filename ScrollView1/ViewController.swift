531953a644f5ce1975d244e3fc45642d38c34a6d
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-06-03 15:33:41 +0700

    Add double tap to zoom at exact postion

--- ScrollView1/ViewController.swift
+++ ScrollView1/ViewController.swift
@@ -19,9 +19,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         scrollView.delegate = self
         scrollView.maximumZoomScale = 10
+        let doubleTap =  UITapGestureRecognizer.init(target: self, action: #selector(doubleTappedHandler(recognizer:)))
+        doubleTap.numberOfTapsRequired = 2
+        scrollView.addGestureRecognizer(doubleTap)
     }
     
-    
+    @objc func doubleTappedHandler(recognizer: UITapGestureRecognizer) {
+        if scrollView.zoomScale == 1 {
+            scrollView.zoom(to: zoomRectForScale(scale: scrollView.maximumZoomScale, center: recognizer.location(in: recognizer.view)), animated: true)
+        } else {
+            scrollView.setZoomScale(1, animated: true)
+        }
+    }
+
+    func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+        zoomRect.size.height = imvImage.frame.size.height / scale
+        zoomRect.size.width  = imvImage.frame.size.width  / scale
+        let newCenter = scrollView.convert(center, from: imvImage)
+        zoomRect.origin.x = newCenter.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = newCenter.y - (zoomRect.size.height / 2.0)
+        return zoomRect
+    }
 }
 
 //MARK: - UIScrollViewDelegate
@@ -30,4 +49,5 @@ extension ViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imvImage
     }
+    
 }
